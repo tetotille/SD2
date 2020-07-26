63914a93d08e6c8eea645813e5debcd7e05e5ce8
alecaceres <61921720+alecaceres@users.noreply.github.com>
2020-07-26 19:39:40 -0400

    Se agregan comentarios a fibo_no_recursivo.asm

--- fibo_no_recursivo.asm
+++ fibo_no_recursivo.asm
@@ -25,27 +25,26 @@ main:
 	syscall 
 	
 fibo:
-	li $t0, 0
-	li $t1, 1
-	li $t2, 0
+	li $t0, 0 # fibo(n-1)
+	li $t1, 1 # fibo(n)
+	li $t2, 0 # contador
 	
-	slti $t4, $a0, 2
-	bne $t4, $0, if
-	
+	slti $t4, $a0, 2 # si n < 2, t4 = 1
+	bne $t4, $0, if # si t4 = 1 (si n < 2)
+	# si n >= 2
 	while:
-		slt $t4, $t2, $a0
-		beq $t4, $0, end
+		slt $t4, $t2, $a0 # t4 = 1 si contador < n
+		beq $t4, $0, end # si contador = n, end
 		
-		addi $t2, $t2, 1
-		add $t3, $t0, $t1
+		addi $t2, $t2, 1 # contador = contador + 1
+		add $t3, $t0, $t1 # t3 = t0+t1
 		move $t0, $t1
 		move $t1, $t3	
 		b while
 	end:
-		move $v0, $t1
+		move $v0, $t1 # t1 = fibo(n)
 		jr $ra
 
 	if:
 		li $v0, 1
 		jr $ra
-
